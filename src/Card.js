35e0f96f7ac64a200159b9602c89cc1ddc9e97f5
PrajapatiVikrant <146848720+PrajapatiVikrant@users.noreply.github.com>
2024-10-24 12:03:41 +0530

    Change resume

--- src/Card.js
+++ src/Card.js
@@ -9,7 +9,7 @@ function Card(props) {
     
     <div className="projectcard">
       <h2>{props.name}</h2>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQukZ-198m1pn-B2gMhuZdeeU0T379lXGC-tg&s" alt="" />
+      <img className="cardimg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQukZ-198m1pn-B2gMhuZdeeU0T379lXGC-tg&s" alt="" />
       <p className="aboutproject">{props.discription}</p>
       <div className="btnctn">
       <a href={props.source1} className="btn">Demo</a>
--- src/Project.js
+++ src/Project.js
@@ -8,25 +8,26 @@ import "./Project.css";
 function Project() {
   return (
     <div className="projectvideo">
-        <br />
+      <br />
       <li>MERN technology</li>
       <br />
       <div className="technology">
-      <Card name = "Socialmedia" source1 = "https://www.linkedin.com/posts/vikrantprajapati_i-have-updated-my-socialmedia-project-activity-7220008967224086528-yvBp?utm_source=share&utm_medium=member_desktop" source2="https://vikrant-socialmedia-frontend.vercel.app" discription = "In this project i have created some features like login and signup with jwt , User can send freind request , user can create connection by accept request , User can read, upload, edit and delete own post. , User can like and comment on that post , User can chat with his/her connection https://github.com/PrajapatiVikrant/SocialmediaBackend (backend) https://github.com/PrajapatiVikrant/SocialmediaFrontend (frontend)"  />
-        <Card name = "Shopping Cart(Meesho clone)" source1 = "https://www.linkedin.com/posts/vikrantprajapati_i-have-created-shopping-cart-activity-7201919365670211585-e-Bq?utm_source=share&utm_medium=member_android" source2="https://my-meesho-frontend.vercel.app/" discription = "In this project i have created some features like login/signUp,display all product with filtering and you can add,delete,edit and read item in own cart and give your order and you also can track your order but this is only for desktop https://github.com/PrajapatiVikrant/ MyMeeshoFrontend https://github.com/PrajapatiVikrant/ MeeshoBackend"  />
-        <Card name = "Text Editor" source1 = "https://www.linkedin.com/posts/vikrantprajapati_i-have-created-file-management-system-activity-7201896160947572736-dJiu?utm_source=share&utm_medium=member_android" source2="https://text-editor-vikrant.vercel.app/" discription = 'I have created this project using MERN technology in which we can do CRUD operation. https://github.com/PrajapatiVikrant/ Text-Editor ( frontend ) https://github.com/PrajapatiVikrant/ Texteditor- (backend)'/>
-        <Card name = "TimeShipping" source1 = "https://www.linkedin.com/posts/vikrantprajapati_i-have-created-timeshipping-system-activity-7201945227203387392-Hfsc?utm_source=share&utm_medium=member_android" source2="https://time-shipping-frontend.vercel.app/" discription = 'I have created this project using MERN technology in which user can login/signup as employee/manager in which employee can send project data and can see all sent data by himself but employee cannot change sent data and as manager user can see all data sent by employee and user is also authorised person for delete data and give rate of work. https://github.com/PrajapatiVikrant/ TimeShipping-Frontend https://github.com/PrajapatiVikrant/ TimeShipping (backend)'/>
-        <Card name = "Todo list" source1 = "https://w3dev-assignment-frontend.vercel.app/" source2 = "https://w3dev-assignment-frontend.vercel.app/" discription = "In this you can login/signUp and after that you can perform crud(create,read,update,delete) operation."  />
+        <Card name="Socialmedia" source1="https://www.linkedin.com/posts/vikrantprajapati_i-have-updated-my-socialmedia-project-activity-7220008967224086528-yvBp?utm_source=share&utm_medium=member_desktop" source2="https://vikrant-socialmedia-frontend.vercel.app" discription="In this project i have created some features like login and signup with jwt , User can send freind request , user can create connection by accept request , User can read, upload, edit and delete own post. , User can like and comment on that post , User can chat with his/her connection." />
+        <Card name="Shopping Cart(Meesho clone)" source1="https://www.linkedin.com/posts/vikrantprajapati_i-have-created-shopping-cart-activity-7201919365670211585-e-Bq?utm_source=share&utm_medium=member_android" source2="https://my-meesho-frontend.vercel.app/" discription="In this project i have created some features like login/signUp,display all product with filtering and you can add,delete,edit and read item in own cart and give your order and you also can track your order but this is only for desktop" />
+        <Card name="Text Editor" source1="https://www.linkedin.com/posts/vikrantprajapati_i-have-created-file-management-system-activity-7201896160947572736-dJiu?utm_source=share&utm_medium=member_android" source2="https://text-editor-vikrant.vercel.app/" discription='I have created this project using MERN technology in which we can do CRUD operation.' />
+        <Card name="TimeShipping" source1="https://www.linkedin.com/posts/vikrantprajapati_i-have-created-timeshipping-system-activity-7201945227203387392-Hfsc?utm_source=share&utm_medium=member_android" source2="https://time-shipping-frontend.vercel.app/" discription='I have created this project using MERN technology in which user can login/signup as employee/manager in which employee can send project data and can see all sent data by himself but employee cannot change sent data and as manager user can see all data sent by employee and user is also authorised person for delete data and give rate of work.' />
+        <Card name="Todo list" source1="https://w3dev-assignment-frontend.vercel.app/" source2="https://w3dev-assignment-frontend.vercel.app/" discription="In this you can login/signUp and after that you can perform crud(create,read,update,delete) operation." />
       </div>
       <br />
       <li>React Project</li>
       <br />
       <div className="technology">
-        <Card name = "React template" source1 = "https://vikrant-assiment.vercel.app/" source2="https://vikrant-assiment.vercel.app/" discription = "This is a static react template.In this project i have used reusable componants to design this template"  />
+        <Card name="React template" source1="https://vikrant-assiment.vercel.app/" source2="https://vikrant-assiment.vercel.app/" discription="This is a static react template.In this project i have used reusable componants to design this template" />
+        <Card name="Accuknox assignment" source1="https://accuknox-assignment-one.vercel.app/" source2="https://accuknox-assignment-one.vercel.app/" discription="This is crud project. This project include multiple category and user can add , remove , edit , read any widget in any category and user can search any widget by name. " />
       </div>
-  
 
-     
+
+
     </div>
   );
 }
